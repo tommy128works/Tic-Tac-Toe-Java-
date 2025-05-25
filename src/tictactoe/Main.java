fc6a7b43889f8249da42bff5a8e89b0e9dc4048c
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-05-25 13:51:50 -0400

    Print gameboard based on text input

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,10 +1,26 @@
 package tictactoe;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+//        String gameInput = scanner.nextLine();
+        String gameInput = "XXXOOOXXX";
+        scanner.close();
+
+        if (gameInput.length() != 9) {
+            System.out.println("Invalid input: incorrect length");
+            System.exit(1);
+        }
+
+        System.out.println(gameInput);
+
+        System.out.println("---------");
+        System.out.println("| " + gameInput.charAt(0) + " " + gameInput.charAt(1) + " " + gameInput.charAt(2) + " |");
+        System.out.println("| " + gameInput.charAt(3) + " " + gameInput.charAt(4) + " " + gameInput.charAt(5) + " |");
+        System.out.println("| " + gameInput.charAt(6) + " " + gameInput.charAt(7) + " " + gameInput.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
